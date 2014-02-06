8fab090598399e3d92c81e5bc92c0ba8ba125f05
Aaron Trostle <aaron.trostle@gmail.com>
2014-02-05 21:37:25 -0800

    refactor problem 2

--- problem2.rb
+++ problem2.rb
@@ -1,16 +1,21 @@
-counter = 1
-num = 1
-div = 1
+def smallest_evenly_divisible_by(range)
+  counter = range.begin
+  max = range.end + 1
+  current_num = 1
+  divisor = 1
 
-while counter != 21 do
-  if num % div == 0
-    div += 1
-    counter += 1
-  else
-    counter = 1
-    div = 1
-    num += 1
+  while counter != max do
+    if current_num % divisor == 0
+      divisor += 1
+      counter += 1
+    else
+      counter = range.begin
+      divisor = 1
+      current_num += 1
+    end
   end
+  puts "#{current_num} is the smallest number that can be divided by each of the numbers from #{range.begin} to #{range.end} without any remainder."
 end
 
-puts num
+smallest_evenly_divisible_by(1..20)
+
